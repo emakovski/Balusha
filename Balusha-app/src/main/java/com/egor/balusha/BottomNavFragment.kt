2ca0bcc2129ddcc1b165c8f97d77be21b6e2ee12
emakovski <egorushka.man@gmail.com>
2021-04-18 12:01:26 +0300

    -

---
+++ Balusha-app/src/main/java/com/egor/balusha/BottomNavFragment.kt
@@ -0,0 +1,49 @@
+package com.egor.balusha
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_bottom_menu.*
+
+class BottomNavFragment: BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottom_menu, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            // Bottom Navigation Drawer menu item clicks
+            when (menuItem.itemId) {
+                R.id.menu_my_pet -> context!!.toast(getString(R.string.nav1_clicked))
+                R.id.menu_vaccination -> context!!.toast(getString(R.string.nav2_clicked))
+                R.id.menu_helminths -> context!!.toast(getString(R.string.nav3_clicked))
+                R.id.menu_fleas -> context!!.toast(getString(R.string.nav1_clicked))
+                R.id.menu_reproduction -> context!!.toast(getString(R.string.nav2_clicked))
+                R.id.menu_diet -> context!!.toast(getString(R.string.nav3_clicked))
+                R.id.menu_places -> context!!.toast(getString(R.string.nav1_clicked))
+                R.id.menu_moments -> context!!.toast(getString(R.string.nav2_clicked))
+                R.id.menu_weather -> context!!.toast(getString(R.string.nav3_clicked))
+                R.id.menu_navi -> context!!.toast(getString(R.string.nav3_clicked))
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            true
+        }
+    }
+
+    // This is an extension method for easy Toast call
+    fun Context.toast(message: CharSequence) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 600)
+        toast.show()
+    }
+
+}
--- Balusha-app/src/main/java/com/egor/balusha/MainActivity.kt
+++ Balusha-app/src/main/java/com/egor/balusha/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.egor.balusha
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fab: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab=findViewById(R.id.fab_main)
+        setSupportActionBar(bottom_app_bar)
+        setFabListener()
     }
+    private fun setFabListener() {
+        fab.setOnClickListener {val bottomNavDrawerFragment = BottomNavFragment()
+            bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+        }
+    }
+
 }
