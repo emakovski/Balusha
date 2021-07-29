c40f5feed38712cf28e08696116f7dbb2cb8a479
egor.makovsky <egor.makovsky@cogniteq.com>
2021-07-29 15:34:00 +0300

    -

--- Balusha-app/src/main/java/com/egor/balusha/activities/FleasTicksAdd.kt
+++ Balusha-app/src/main/java/com/egor/balusha/activities/FleasTicksAdd.kt
@@ -1,19 +1,21 @@
 package com.egor.balusha.activities
 
 import android.app.Activity
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.egor.balusha.DatabaseRepository
 import com.egor.balusha.databinding.FleasAddBinding
-import com.egor.balusha.databinding.FleasAndTicksBinding
-import com.egor.balusha.databinding.HelminthsAddBinding
 import com.egor.balusha.dbpets.FleasInfo
-import com.egor.balusha.dbpets.HelminthsInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.util.*
+
 
 private const val RESULT_CODE_BUTTON_BACK = 3
 
@@ -21,6 +23,9 @@ class FleasTicksAdd : AppCompatActivity() {
     private lateinit var binding: FleasAddBinding
     private lateinit var repository: DatabaseRepository
     private lateinit var activityScope: CoroutineScope
+    private val cal: Calendar = Calendar.getInstance()
+    private var picker: DatePickerDialog? = null
+    private lateinit var date: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +43,34 @@ class FleasTicksAdd : AppCompatActivity() {
         binding.fabFleasAdd.setOnClickListener {
             addFleasInfoAndBackToPreviousActivity()
         }
+        binding.dateInFleasAdd.setOnClickListener{
+//            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+//                cal.set(Calendar.YEAR, year)
+//                cal.set(Calendar.MONTH, monthOfYear)
+//                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+//
+//                val myFormat = "dd.mm.yyyy"
+//                val sdf = SimpleDateFormat(myFormat, Locale.US)
+//                binding.dateInFleasAdd.setText(sdf.format(cal.time))}
+//
+//            DatePickerDialog(this, dateSetListener,
+//                cal.get(Calendar.YEAR),
+//                cal.get(Calendar.MONTH),
+//                cal.get(Calendar.DAY_OF_MONTH)).show()
+
+
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            val month = cal.get(Calendar.MONTH)
+            val yearr = cal.get(Calendar.YEAR)
+            picker = DatePickerDialog(this,
+                { view, year, monthOfYear, dayOfMonth -> binding.dateInFleasAdd.setText(dayOfMonth.toString() + "." + (monthOfYear + 1) + "." + year)}, yearr, month, day)
+            picker!!.show()
+            date=binding.dateInFleasAdd.text.toString()
+        }
     }
 
     private fun addFleasInfoAndBackToPreviousActivity() {
         val name = binding.nameFleasAdd.text.toString()
-        val date = binding.dateInFleasAdd.text.toString()
         if (name.isNotEmpty() && date.isNotEmpty()) {
             val fleasInfo = FleasInfo(name, date)
             //made with coroutines
@@ -64,6 +92,12 @@ class FleasTicksAdd : AppCompatActivity() {
     private fun backToPreviousActivity() {
         setResult(RESULT_CODE_BUTTON_BACK, intent)
         finish()
+    }
+    private fun updateLabel() {
+        val myFormat = "dd.mm.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        binding.dateInFleasAdd.setText(sdf.format(cal.time))
+        //(dayOfMonth.toString() + "." + (monthOfYear + 1) + "." + year)
     }
 
 }
